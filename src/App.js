df3ae871da316f2f8598889c166f71778c12c21f
tosmel2 <adewaletosin0808@gmail.com>
2023-07-01 23:12:14 +0100

    refactor:i install tailwind css and created a new signup and signin pages for testing purposes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Signup from "./pages/sign-up/Signup";
-import Signin from "./pages/sign-in/Signin";
+import Signup from "./pages/sign-up/Signup.jsx";
+import Signin from "./pages/sign-in/Signin.jsx";
 import ConnectWallet from "./pages/connect-wallet/ConnectWallet";
 import Home from "./pages/home/Home";
 import ResetPassword from "./pages/reset-password/ResetPassword";
