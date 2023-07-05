91bc0d4d97c528a9c20f5caf9a1a7a1f5e2ef686
tosmel2 <adewaletosin0808@gmail.com>
2023-07-05 07:11:11 -0700

    notification alert to users on voting for their choice of candidate

--- src/pages/presidential-poll/PresidentialPoll.js
+++ src/pages/presidential-poll/PresidentialPoll.js
@@ -14,6 +14,9 @@ import {
     SearchWrapper,
     VButton
   } from "./presidential.poll.styles.js";
+  import React, { useState } from 'react';
+  import Swal from 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.css';
   import searchIcon from "../../assets/search-icon.png";
   import filter from "../../assets/filter.png";
   import apc from "../../assets/apc.png";
@@ -31,6 +34,23 @@ import {
   
   
   const PresidentialPoll = () => {
+    const [votedCandidate, setVotedCandidate] = useState('');
+
+  const handleVote = (candidate) => {
+    if (!votedCandidate) {
+      Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        fontWeight: 'bold',
+        title: 'Successful',
+        text: 'You have successfully casted your vote to your preferred candidate',
+        showConfirmButton: false,
+        timer: 2000
+      });
+
+      setVotedCandidate(candidate);
+    }
+  };
   
   
     return (
@@ -78,52 +98,80 @@ import {
                     <td>Bola Tinubu</td>
                     <td>All Progressive Congress(APC)</td>
                     <td><img src={apc} alt="apc"/></td>
-                    <td><VButton>Vote</VButton></td>
+                    <td>
+                    <VButton 
+                      onClick={() => handleVote('Bola Tinubu')}
+                      className={votedCandidate === 'Bola Tinubu' ? 'bg-gray-300 cursor-not-allowed' : 'bg-green-500'}
+                      disabled={votedCandidate !== ''}>
+                        Vote
+                    </VButton></td>
                     
                   </tr>
                   <tr>
                     <td>Professor Peter Umeadi</td>
                     <td>Accord Party (AP)</td>
                     <td><img src={accord} alt="accord"/></td>
-                    <td><VButton>Vote</VButton></td>
+                    <td><VButton 
+                      onClick={() => handleVote('Professor Peter Umeadi')}
+                      className={votedCandidate === 'Professor Peter Umeadi' ? 'bg-gray-300 cursor-not-allowed' : 'bg-green-500'}
+                      disabled={votedCandidate !== ''}>Vote</VButton>
+                    </td>
                     </tr>
                   <tr>
                   <td>Sowore Omoyele</td>
                     <td>Peoples Democratic Party (PDP)</td>
                     <td><img src={pdp} alt="pdp"/></td>
-                    <td><VButton>Vote</VButton></td>
+                    <td><VButton 
+                      onClick={() => handleVote('Sowore Omoyele')}
+                      className={votedCandidate === 'Sowore Omoyele' ? 'bg-gray-300 cursor-not-allowed' : 'bg-green-500'}
+                      disabled={votedCandidate !== ''}>Vote</VButton></td>
                     
                   </tr>
                   <tr>
                   <td>Peter Obi</td>
                     <td>Labour Party (LP) </td>
                     <td><img src={lp} alt="lp"/></td>
-                    <td><VButton>Vote</VButton></td>
+                    <td><VButton 
+                      onClick={() => handleVote('Peter Obi')}
+                      className={votedCandidate === 'Peter Obi' ? 'bg-gray-300 cursor-not-allowed' : 'bg-green-500'}
+                      disabled={votedCandidate !== ''}>Vote</VButton></td>
                     </tr>
                   <tr>
                   <td>Rabiu Musa Kwankwaso</td>
                     <td>All Progressives Grand Alliance (APGA)</td>
                     <td><img src={apga} alt="apga"/></td>
-                    <td><VButton>Vote</VButton></td>
+                    <td><VButton 
+                      onClick={() => handleVote('Rabiu Musa Kwankwaso')}
+                      className={votedCandidate === 'Rabiu Musa Kwankwaso' ? 'bg-gray-300 cursor-not-allowed' : 'bg-green-500'}
+                      disabled={votedCandidate !== ''}>Vote</VButton></td>
                     </tr>
                   <tr>
                   <td>Nwanyanwu Daniel</td>
                     <td>African Action Congress (AAC)</td>
                     <td><img src={aac} alt="aac"/></td>
-                    <td><VButton>Vote</VButton></td>
+                    <td><VButton 
+                      onClick={() => handleVote('Nwanyanwu Daniel')}
+                      className={votedCandidate === 'Nwanyanwu Daniel' ? 'bg-gray-300 cursor-not-allowed' : 'bg-green-500'}
+                      disabled={votedCandidate !== ''}>Vote</VButton></td>
                     </tr>
                   <tr>
                   <td>Imumolen Christopher</td>
                     <td>Action Alliance (AA)</td>
                     <td><img src={aa} alt="aa"/></td>
-                    <td><VButton>Vote</VButton></td>
+                    <td><VButton 
+                      onClick={() => handleVote('Imumolen Christopher')}
+                      className={votedCandidate === 'Imumolen Christopher' ? 'bg-gray-300 cursor-not-allowed' : 'bg-green-500'}
+                      disabled={votedCandidate !== ''}>Vote</VButton></td>
                     
                   </tr>
                   <tr>
                   <td>Dumebi Kachikwu</td>
                     <td>Zenith Labour Party (ZLP)</td>
                     <td><img src={zlp} alt="zlp"/></td>
-                    <td><VButton>Vote</VButton></td>
+                    <td><VButton 
+                      onClick={() => handleVote('Dumebi Kachikwu')}
+                      className={votedCandidate === 'Dumebi Kachikwu' ? 'bg-gray-300 cursor-not-allowed' : 'bg-green-500'}
+                      disabled={votedCandidate !== ''}>Vote</VButton></td>
                     
                   </tr>
   
